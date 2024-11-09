9c9d9aa2d4d7e42cf8cff618ab76fa8903688f9c
Rentaro Suzuki <rentajaxa96@gmail.com>
2024-11-09 16:58:38 +0900

    顔文字ボタンの追加

--- TechTap/ViewController.swift
+++ TechTap/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 0 ..< 100 {
+            let button = addButton(on: view)
+            button.titleLabel?.font = UIFont.systemFont(ofSize:32)
+            button.setTitle("🥺", for: .normal)
+            button.tag = i
+            button.addAction(
+                .init(handler: {action in
+                    let sender = action.sender as! UIButton
+                    self.buttonTapped(sender)
+                }), for: .touchUpInside
+            )
+        }
+        
+        }
         // Do any additional setup after loading the view.
+    func buttonTapped(_ sender: UIButton) {
+        print("\(sender.tag)の絵文字が押されたよ！")
+        
+        sender.removeFromSuperview()
+    }
     }
 
 
-}
+
 
