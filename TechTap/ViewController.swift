1f256e05b5f8f2a20f95bffe7674dfd4b209bc42
Rentaro Suzuki <rentajaxa96@gmail.com>
2024-11-09 16:35:46 +0900

    Initial Commit

---
+++ TechTap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechTap
+//
+//  Created by 鈴木廉太郎 on 2024/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
